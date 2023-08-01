40222a98f5648b0cd75cb4dc50252f2275b62c74
rohit789@ <princeshmr27@gmail.com>
2023-08-02 01:38:38 +0530

    Add more than one person from AddressBookMain class

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,10 +6,16 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
 		AddressBook addressBook=new AddressBook();
-		addressBook.addContacts();   //add contact only for 1 person contact
+		//addressBook.addContacts();   //add contact only for 1 person contact
+		//addressBook.displayContact();
+		
+		 Scanner scan=new Scanner(System.in);
+		 System.out.println("Enter the number of contact you want to add: ");
+		 int numberOfContacts=scan.nextInt();
+	   	for(int i=0; i<numberOfContacts; i++) {
+				addressBook.addContacts();
+		}
 		addressBook.displayContact();
-		
-		
 
 	}
 
